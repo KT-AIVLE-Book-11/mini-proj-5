b05db7edda00e07c424e689604effe55036da5a3
kyjin <kyjin320@sookmyung.ac.kr>
2026-06-09 13:19:56 +0900

    init: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/ki/aivle/mini_proj5/MiniProj5Application.java
@@ -0,0 +1,13 @@
+package ki.aivle.mini_proj5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProj5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProj5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ki/aivle/mini_proj5/MiniProj5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ki.aivle.mini_proj5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProj5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
