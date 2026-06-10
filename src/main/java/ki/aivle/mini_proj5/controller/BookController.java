ab584afe4a63f80473f0cfb1b157b7b7ffd57b2c
onestar <star4752.0528@gmail.com>
2026-06-10 13:23:19 +0900

    fix: BookController API 기본 주소 변경 (/books -> /api/v1/books)

--- src/main/java/ki/aivle/mini_proj5/controller/BookController.java
+++ src/main/java/ki/aivle/mini_proj5/controller/BookController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/v1/books")
 @RequiredArgsConstructor
 public class BookController {
 
