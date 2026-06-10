87d35236893abf37b7dae7828606e26c3e5832c9
bch0714@naver.com <bch0714@naver.com>
2026-06-10 13:10:28 +0900

    BookService 수정

--- src/main/java/ki/aivle/mini_proj5/service/BookService.java
+++ src/main/java/ki/aivle/mini_proj5/service/BookService.java
@@ -59,7 +59,7 @@ public class BookService {
         return bookRepository.save(existing);
     }
 
-    // 도서 수정 - 표지
+    // 도서 수정 - 표지 (프롬프트 사용안하는 변수라 삭제)
     @Transactional
     public Book updateCover(Long id, String coverImageUrl) {
         Book existing = getById(id);
@@ -91,5 +91,41 @@ public class BookService {
         Book book = getById(id);
         book.setLikes(book.getLikes() + 1);
     }
+
+    // 프론트 엔드 처리
+    /*
+    // 조회수 증가
+    @Transactional
+    public void incrementViews(Long id, int views) {
+        Book book = getById(id);
+        book.setViews(views);
+    }
+
+    // 좋아요 증가
+    @Transactional
+    public void incrementLikes(Long id, int likes) {
+        Book book = getById(id);
+        book.setLikes(likes);
+    }
+     */
+
+    // 백엔드 처리
+    /*
+    // 조회수 증가
+    @Transactional
+    public int incrementViews(Long id) {
+        Book book = getById(id);
+        book.setViews(book.getViews() + 1);
+        return book.getViews();
+    }
+
+    // 좋아요 증가
+    @Transactional
+    public int incrementLikes(Long id) {
+        Book book = getById(id);
+        book.setLikes(book.getLikes() + 1);
+        return book.getLikes();
+    }
+    */
 
 }
